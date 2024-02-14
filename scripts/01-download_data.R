c1f66333364f2ae48c535ee97db0e8574c3deb6c
Mohammed Yusuf Shaikh <yusuf.mns@icloud.com>
2024-02-14 20:10:22 +0000

    Commit Download Script & Input file

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,44 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data input 5 file are present
+# Author: Mohammed Yusuf Shaikh
+# Date: 11 February 2024
+# Contact: mohammedyusuf.shaikh@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Change Directory for running the code
 
-
-#### Workspace setup ####
-library(opendatatoronto)
+#### Loading Workspace ####
+# Load necessary libraries
+library(dplyr)
+library(readr)
+library(haven)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(janitor)
+library(knitr)
 
-#### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+#### Downloading Data in Script ####
 
+# Set working directory to where the data files are located
+setwd("/cloud/project/inputs/data") # Replace with your actual path to the data files
 
+# Read the data files
 
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+eurostat <- read_csv("Eurostat.csv")
+market_share <- read_csv("market_share.csv")
+specification_data <- read_dta("specification_data.dta")
+temp_data <- read_dta("temp_data.dta")
+bgd2 <- read_dta("bgd2.dta")
+eu_countries <- read_dta("eu_countries.dta")
+
+# Set the path to the input folder where the files will be saved
+input_folder_path <- "/cloud/project/inputs/data"
+
+# Write the files to the input folder
+write_dta(eu_countries, file.path(input_folder_path, "eu_countries.dta"))
+write_csv(eurostat, file.path(input_folder_path, "Eurostat.csv"))
+write_csv(market_share, file.path(input_folder_path, "market_share.csv"))
+write_dta(specification_data, file.path(input_folder_path, "specification_data.dta"))
+write_dta(temp_data, file.path(input_folder_path, "temp_data.dta"))
+write_dta(bgd2, file.path(input_folder_path, "bgd2.dta"))
+
 
          
